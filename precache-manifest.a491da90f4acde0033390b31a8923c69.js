44c647347c2c62027f13898e986121cbd954b185
Prasad Chaudhari <prasadchaudhari117@gmail.com>
2020-10-21 17:23:11 +0530

    Updates

---
+++ precache-manifest.a491da90f4acde0033390b31a8923c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f09304fe5c091fcf5fb4e4db85fd11",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/index.html"
+  },
+  {
+    "revision": "6d2e33fcc59e45e8a047",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/static/css/main.6dc1deef.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2e33fcc59e45e8a047",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/static/js/main.5e242992.chunk.js"
+  },
+  {
+    "revision": "ae142a59516c5ed85a5e",
+    "url": "/Prasadprd.github.io/MonstersRolodexProject/static/js/runtime-main.f6bcf97e.js"
+  }
+]);
